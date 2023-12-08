8cf588c46a63ff725ae09815dfdd8efb79d68758
Jose <ls.joseignacio@gmail.com>
2023-12-08 11:44:21 +0100

    feat: clean up comment

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -18,28 +18,6 @@ export const UserProvider = ({ children }: { children: ReactElement }) => {
   const [name, setName] = useState<string | undefined>(undefined);
   const [token, setToken] = useState<string | undefined>(undefined);
 
-  // const login = async (name: string, password: string) => {
-  //   const response = await request(`${apiUrl}users/login`, {
-  //     method: ERequestMethods.POST,
-  //     body: { name, password },
-  //   });
-  //   console.log(response)
-  //   if (response.status === 200) {
-  //     setName(name);
-  //     setToken(response.data.token);
-  //   }
-  // };
-
-  // const loggout = async () => {
-  //   if (!name) return;
-  //   setName(undefined);
-  //   setToken(undefined);
-  // };
-
-  // useEffect(() => {
-  //   login("jose", "1234");
-  // }, []);
-
   return (
     <userContext.Provider
       value={{
