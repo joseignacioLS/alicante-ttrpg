8025f3c2f54e4895095e94edda70da24cca41db5
Jose <ls.joseignacio@gmail.com>
2023-12-08 21:49:04 +0100

    fix: types

--- src/app/manager/[id]/page.tsx
+++ src/app/manager/[id]/page.tsx
@@ -2,7 +2,7 @@ import GamePage from "@/components/GamePage";
 import React from "react";
 
 const Home = () => {
-  return <GamePage backRoute="/manager" managerMode={true} />;
+  return <GamePage backRoute="/manager" />;
 };
 
 export default Home;
